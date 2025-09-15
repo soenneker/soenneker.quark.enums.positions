f21b7483ffaae5bedccede6499f7b4918994f64d
Jake Soenneker <jake@soenneker.com>
2025-09-15 17:13:11 -0500

    Initial commit

---
+++ src/Position.cs
@@ -0,0 +1,38 @@
+﻿using Intellenum;
+using Soenneker.Quark.Enums.GlobalKeywords;
+
+namespace Soenneker.Quark.Enums.Positions;
+
+/// <summary>
+/// An HTML CSS keyword set in .NET enumeration form
+/// </summary>
+[Intellenum<string>]
+public sealed partial class Position
+{
+    /// <summary>
+    /// Default static positioning.
+    /// </summary>
+    public static readonly Position Static = new("static");
+
+    /// <summary>
+    /// Positioned relative to its normal position.
+    /// </summary>
+    public static readonly Position Relative = new("relative");
+
+    /// <summary>
+    /// Positioned relative to the nearest positioned ancestor.
+    /// </summary>
+    public static readonly Position Absolute = new("absolute");
+
+    /// <summary>
+    /// Positioned relative to the viewport.
+    /// </summary>
+    public static readonly Position Fixed = new("fixed");
+
+    /// <summary>
+    /// Positioned based on the user's scroll position.
+    /// </summary>
+    public static readonly Position Sticky = new("sticky");
+
+    public static implicit operator Position(GlobalKeyword style) => new(style.Value);
+}
---
+++ test/Soenneker.Quark.Enums.Positions.Tests/PositionTests.cs
@@ -0,0 +1,13 @@
+﻿using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Quark.Enums.Positions.Tests;
+
+public sealed class PositionTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
