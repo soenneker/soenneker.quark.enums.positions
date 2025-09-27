cf540b8b5221114a5f08319ba5161b9652abf8c4
Jake Soenneker <jake@soenneker.com>
2025-09-27 09:03:20 -0500

    rename

--- src/PositionKeyword.cs
+++ src/PositionKeyword.cs
@@ -6,32 +6,32 @@ namespace Soenneker.Quark.Enums;
 /// An HTML CSS keyword set in .NET enumeration form
 /// </summary>
 [Intellenum<string>]
-public sealed partial class Position
+public sealed partial class PositionKeyword
 {
     /// <summary>
     /// Default static positioning.
     /// </summary>
-    public static readonly Position Static = new("static");
+    public static readonly PositionKeyword Static = new("static");
 
     /// <summary>
     /// Positioned relative to its normal position.
     /// </summary>
-    public static readonly Position Relative = new("relative");
+    public static readonly PositionKeyword Relative = new("relative");
 
     /// <summary>
     /// Positioned relative to the nearest positioned ancestor.
     /// </summary>
-    public static readonly Position Absolute = new("absolute");
+    public static readonly PositionKeyword Absolute = new("absolute");
 
     /// <summary>
     /// Positioned relative to the viewport.
     /// </summary>
-    public static readonly Position Fixed = new("fixed");
+    public static readonly PositionKeyword Fixed = new("fixed");
 
     /// <summary>
     /// Positioned based on the user's scroll position.
     /// </summary>
-    public static readonly Position Sticky = new("sticky");
+    public static readonly PositionKeyword Sticky = new("sticky");
 
-    public static implicit operator Position(GlobalKeyword style) => new(style.Value);
+    public static implicit operator PositionKeyword(GlobalKeyword style) => new(style.Value);
 }
