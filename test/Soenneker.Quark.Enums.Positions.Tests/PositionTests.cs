b22d5405fed2136264c53d99db86baadfeaf1cef
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:29 -0500

    TUnit conversion

--- test/Soenneker.Quark.Enums.Positions.Tests/PositionTests.cs
+++ test/Soenneker.Quark.Enums.Positions.Tests/PositionTests.cs
@@ -1,11 +1,10 @@
 using Soenneker.Tests.Unit;
-using Xunit;
 
 namespace Soenneker.Quark.Enums.Positions.Tests;
 
 public sealed class PositionTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
